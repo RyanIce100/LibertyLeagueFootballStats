effaa75596a702da504e9b33f16df92672467d1d
RyanIce100 <183541353+RyanIce100@users.noreply.github.com>
2026-06-14 19:56:31 -0400

    Update DataFixes.R

--- ScrapingFunctions/DataFixes.R
+++ ScrapingFunctions/DataFixes.R
@@ -3,14 +3,16 @@ LLData_final_fixed <- LLData_final_fixed %>%
   slice(-(4766:4794))
 LLData_final_fixed <- LLData_final_fixed[-c(4751,4752,4753,4754,4755,4756,4757,4763,4764,4765), ]
 LLData_final_fixed <- LLData_final_fixed[-c(3919,3969,4029,4094,5702), ]
-LLData_final_fixed <- LLData_final_fixed[-c(4393,4452), ] ##I have no fucking clue why this isn't working
-LLData_final_fixed <- LLData_final_fixed[-c(3866,5976,5977), ] ##I have no fucking clue why this isn't working
-##Last player checked on list sorted from most games: Amaechi Konkwo
 
-LLDataCombined <- LLData_final_fixed |>
+library(dplyr)
+LLData_final_fixed_clean <- LLData_final_fixed |>
+  distinct(Player,Season, .keep_all = TRUE)
+LLDataCombined <- LLData_final_fixed_clean |>
   group_by(Player, Team) |>
   summarize(
     games_played = sum(GP, na.rm = TRUE),
     first_season = min(Season, na.rm = TRUE),
   ) |>
   ungroup()
+
+write.csv(LLData_final_fixed_clean, "LLData_final.csv", row.names = FALSE)
